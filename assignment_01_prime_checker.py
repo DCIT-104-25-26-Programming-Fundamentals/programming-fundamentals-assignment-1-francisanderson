7d9d436c54fd60657f26c169010ba8ecaf41e016
Francis Anderson <32135490+francisanderson@users.noreply.github.com>
2026-07-28 14:41:04 +0000

    code: complete prime checker

--- assignment_01_prime_checker.py
+++ assignment_01_prime_checker.py
@@ -36,22 +36,26 @@
 # =============================================================================
 
 def is_prime(number):
+#Check if number is less than 2. If true, automatically it is not prime. Function ends and returns false.
+#If it is not less than 2, function proceeds to next if
     if number < 2:
         return False
-
+#Check if number is equal to 2. If true it is prime. Function ends and returns true. if greater than 2, function proceeds
+#to the for loop
     if number == 2:
         return True
-
+#In the for loop it starts from 2 to the number n-1. the loops repeats remainder checking by dividing number by (2,3,4...all the
+# way to n-1). if there is no remainder then it is not prime and returns false. if there is a remainder it returns true
     for i in range(2, number):
         if number % i == 0:
             return False
 
     return True
-
-
+#take input
 number = int(input("Enter a number: "))
-
+#if the function isprime is true, print "number" is a prime munber
 if is_prime(number):
     print(f"{number} is a prime number.")
+#if the function isprime is anything else which is false, print "number" is not a prime munber
 else:
     print(f"{number} is NOT a prime number.")
